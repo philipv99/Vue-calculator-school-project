76ad5e0db999edb414d82b3845f2c9c52895d51a
philipv99 <philipv9913@gmail.com>
2023-10-17 22:46:27 +0200

    done

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 Vue.createApp({
    data(){
       return{
-
+         number1: 1,
+         number2: 2,
+         result: 0,
+         operation: 1
       };
    },
    methods: {
-
+      calcNumbers(){
+         if (this.operation == 1){
+            this.result = this.number1 + this.number2 
+         }
+         if (this.operation == 2){
+            this.result = this.number1 - this.number2 
+         }
+         if (this.operation == 3){
+            this.result = this.number1 * this.number2 
+         }
+         if (this.operation == 4){
+            this.result = this.number1 / this.number2 
+         }
+         if (this.operation == 5){
+            this.result = this.number1 ^ this.number2
+            Math.pow() 
+         }
+      }
    }
 }).mount("#app")
